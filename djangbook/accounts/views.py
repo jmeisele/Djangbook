7e4a4d25a228a4982ff04d6ae404cfc836c5a7ae
JASON EISELE <jason.eisele@daimler.com>
2019-10-08 07:13:46 -0500

    created signup and login forms for accounts app

--- djangbook/accounts/views.py
+++ djangbook/accounts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.core.urlresolvers import reverse_lazy
+from django.urls import reverse_lazy
 from . import forms
 from django.views.generic import CreateView
 
 # Create your views here.
 class SignUp(CreateView):
-    pass
+    form_class = forms.UserCreateForm
+    success_url = reverse_lazy('login') #Does not execute until submit done on form
+    template_name = 'accounts/signup.html'
