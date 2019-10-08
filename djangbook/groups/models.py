cb945be3af714dcc187aec0fa8bb5549968d6217
JASON EISELE <jason.eisele@daimler.com>
2019-10-08 09:38:13 -0500

    group_detail.html

--- djangbook/groups/models.py
+++ djangbook/groups/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.utils.text import slugify #Allows removal of characters that aren't alphanumeric
+from django.urls import reverse
 
 # Create your models here.
+import misaka
+from django.contrib.auth import get_user_model
+User = get_user_model() #Allows us to call things off current users session
+from django import template
+register = template.Library()
+
+class Group(models.Model):
+    name = models.CharField(max_length=255, unique = True)
+    slug = models.SlugField(allow_unicode = True, unique = True)
+    decription = models.TextField(blank = True, default = '')
+    description_html = models.TextField(editable = False, default = '', blank = True)
+    members = models.ManyToManyField(User, through = 'GroupMember')
+
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        self.description_html = misaka.html(self.description)
+        super().save(*args, **kwargs)
+    
+    def get_absolute_url(self):
+        return reverse('groups:single', kwargs = {'slug': self.slug})
+    
+    class Meta:
+        ordering = ['name']
+
+
+class GroupMembers(models.Model):
+    group = models.ForeignKey(Group, related_name = 'memberships', on_delete = 'cascade')
+    user = models.ForeignKey(User, related_name = 'user_groups', on_delete = 'cascade')
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        unique_together = ('group', 'user')
+     
+
---
+++ djangbook/posts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from . import views
+
+app_name ='posts'
+
+# Create your url patterns here
+urlpatterns = [
+
+]
