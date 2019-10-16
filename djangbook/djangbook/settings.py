a71889a6cbb1358ea3b7d1c583029503b0002538
JASON EISELE <jason.eisele@daimler.com>
2019-10-16 12:03:22 -0500

    fixed posts delete confirmation and css styling

--- djangbook/djangbook/settings.py
+++ djangbook/djangbook/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    # 'bootstrap4',
     'bootstrap3',
     'groups',
     'posts',
@@ -124,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 LOGIN_REDIRECT_URL = 'test'
 LOGOUT_REDIRECT_URL = 'thanks'
